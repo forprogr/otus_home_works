61b069e5fdbe59aea2ca3765b3602fdc6d3f700a
John Y <for-progr@yandex.ru>
2019-04-03 17:09:25 +0300

    Otus. Домашнее задание #1

---
+++ hw01-maven/src/main/java/ru/forprogr/hw/hw01maven/Main.java
@@ -0,0 +1,9 @@
+package ru.forprogr.hw.hw01maven;
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.print("Return guava joiner:\n");
+		System.out.print(HelloOtus.get_guava_joiner());
+	}
+}
