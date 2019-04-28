4281ec3742a9e34208890ba64cf6f228906d01c8
John Y <for-progr@yandex.ru>
2019-04-28 12:57:18 +0300

    - настройка maven для нового модуля

---
+++ hw03-testing-framework/src/main/java/ru/forprogr/hw/hw03testingframework/Main.java
@@ -0,0 +1,14 @@
+package ru.forprogr.hw.hw03testingframework;
+//-----------------------------------------------------------------------------
+// Author:    Nemti
+// Created:   28.04.2019 11:44
+// Copyright: (c) Nemti 2019
+// Licence:   GPL 3.0
+//-----------------------------------------------------------------------------
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("hw03testingframework");
+	}
+}
