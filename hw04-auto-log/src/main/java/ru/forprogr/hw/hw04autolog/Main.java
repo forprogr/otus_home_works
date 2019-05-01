c75e5576f2ee598bf0aea315c6d2e79291cf7bd5
John Y <for-progr@yandex.ru>
2019-05-01 20:08:50 +0300

    - настройка maven для нового модуля

---
+++ hw04-auto-log/src/main/java/ru/forprogr/hw/hw04autolog/Main.java
@@ -0,0 +1,13 @@
+package ru.forprogr.hw.hw04autolog;
+//-----------------------------------------------------------------------------
+// Author:    Nemti
+// Created:   01.05.2019 20:06
+// Copyright: (c) Nemti 2019
+// Licence:   GPL 3.0
+//-----------------------------------------------------------------------------
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("ru.forprogr.hw.hw04autolog");
+	}
+}
